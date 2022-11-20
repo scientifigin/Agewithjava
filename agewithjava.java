3117a39348529cc136c6ffb83611d21f046eec9a
GOPAL SAHA <112541952+gopalsahagsdev@users.noreply.github.com>
2022-11-20 18:35:04 +0530

    Update agewithjava.java
    Signed-off-by: GOPAL SAHA <112541952+gopalsahagsdev@users.noreply.github.com>

--- agewithjava.java
+++ agewithjava.java
@@ -3,6 +3,9 @@ package scientifigin;
 public class Secondjava {
     public static void main(String[] args) {
         byte age = 34;
+        char ch = 'A';
+        float f1 = 5.7f;
+        double d1 = 4.55d;
         System.out.println(age);
     }
 }
